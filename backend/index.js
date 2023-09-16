ec439a502ff920221ad55f7dc53559726c3a4232
rizannsoophy <rizannsoophy@gmail.com>
2023-09-17 03:32:14 +0530

    sec

--- backend/index.js
+++ backend/index.js
@@ -37,10 +37,10 @@ app.use(errorHandler)
 const __dirname1 = path.resolve();
 
 if (process.env.NODE_ENV = "production") {
-  app.use(express.static(path.join(__dirname1, "../chat_frontend/build")));
+  app.use(express.static(path.join(__dirname1, "../frontend/build")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "chat_frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
   app.get("/", (req, res) => {
